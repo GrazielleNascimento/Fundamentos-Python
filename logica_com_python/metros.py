627eb06546d0dad8235ebe850ab31f03023aa4a6
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-19 09:33:03 -0300

    feat: exercicio

---
+++ logica_com_python/metros.py
@@ -0,0 +1,10 @@
+print('{:*^50}'.format('The Meters Convector'))
+
+meters = float(input('\nEnter the value in meters: '))
+centimeters = meters * 100
+millimeters = meters * 1000
+
+
+
+print('Centimeters: {}'.format(centimeters))
+print('Millimiters: {}'.format(millimeters))
