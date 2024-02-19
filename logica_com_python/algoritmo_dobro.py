dabb431163f8d370ffaa84d81a6efcead9112e66
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-19 09:18:24 -0300

    feat: exercicio com exponenciacao

---
+++ logica_com_python/algoritmo_dobro.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um numero: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz_quadrada = numero ** (0.5)
+print('\n O dobro de {} é {} \n o triplo é {} \n a raiz quadrada é {:.2f}  '.format(numero, dobro, triplo, raiz_quadrada))
