ac63ef79bcf9b60ca521d815fc94d2af206c0fa0
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-19 09:32:57 -0300

    feat: exercicio

---
+++ logica_com_python/media.py
@@ -0,0 +1,5 @@
+first_grade = float(input('Please enter your first grade:  '))
+second_grade = float(input('Please enter your second grade: '))
+average_score = (first_grade + second_grade)/2
+
+print('The Average Score is: {:.2f} '.format(average_score))
