c32eb83e8600d4b28888639a2eba542898b9910c
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-19 09:17:04 -0300

    feat: formatacao

---
+++ logica_com_python/format.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite a sua idade: '))
+altura = float(input('Digite sua altura: '))
+
+print('Aluna {} tem a idade de {} anos, com a altura de {}'.format(nome, idade, altura))
