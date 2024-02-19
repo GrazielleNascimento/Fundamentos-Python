a85f2d63668e9f36676143404bfa2e8d2a6f48cf
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-19 09:18:46 -0300

    feat: sucessor e antecessor

---
+++ logica_com_python/antecessoresucessor.py
@@ -0,0 +1,5 @@
+numero = int(input('Informe um numero: '))
+antecessor = numero - 1
+sucessor = numero + 1
+print(f'Analisando o valor {numero}, seu antecessor é {antecessor} e o seu sucesssor é {sucessor}')
+
