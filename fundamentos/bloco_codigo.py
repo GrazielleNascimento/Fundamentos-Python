8dd8c0f0f0d74d6b980353392c4a4c23cf8e3e8f
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-19 09:16:25 -0300

    feat: identacao

---
+++ fundamentos/bloco_codigo.py
@@ -0,0 +1,9 @@
+def sacar(self, valor: float) -> None:
+    
+    if self.saldo >= valor: #Inicio do bloco do IF
+
+        self.saldo -= valor
+
+    #Fim do bloco do if
+ 
+#Fim do bloco do metodo
