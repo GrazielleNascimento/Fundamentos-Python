2a3396239389512b498a40cc1aa70613d22f9cd3
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-20 19:33:34 -0300

    feat: add exercicio condicionais

---
+++ condicionais/menor_maiorvalor.py
@@ -0,0 +1,6 @@
+primeiro_valor = int(input('Digite o primeiro valor: '))
+segundo_valor = int(input('Digite o segundo valor: '))
+terceiro_valor = int(input('Digite o terceiro valor: '))
+
+# if(segundo_valor< ):
+    # print('O primeiro valor é maior que o segundo')
