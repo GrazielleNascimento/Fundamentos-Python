118c57746375841ba2460780d1fc6d76a0cef0b1
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-24 12:35:00 -0300

    feat: funcoes em python

---
+++ funcoes/soma.py
@@ -0,0 +1,13 @@
+def soma(a,b):
+  resultado = a + b
+  return resultado
+    
+resultado = soma(5,5)
+print(resultado)
+
+# def soma(a,b):
+#   resultado = a + b
+#   print(resultado)
+
+# soma(5,5)
+
