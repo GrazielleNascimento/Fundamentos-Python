5be2a7ff500387b6c35f3b641151fe6755521864
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-24 12:37:07 -0300

    feat: modulo math em python

---
+++ modulos/ex01.py
@@ -0,0 +1,10 @@
+from math import sqrt, ceil
+
+num = int(input('Digite um numero: '))
+
+raiz = sqrt(num)
+
+print(f'A raiz de {num} é igual a {ceil(raiz)}')
+
+# math.ceil - arredonda para cima
+# math.floor - arrendoda para baixo
