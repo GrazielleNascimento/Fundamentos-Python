20e259fb8321b26f34870189c751ce7ca09ebb71
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-03-05 07:32:28 -0300

    add exercicio

---
+++ repeticao_for/ex10.py
@@ -0,0 +1,6 @@
+numeros = []  # Criação da lista para armazenar os números
+
+for c in range(0,3):
+   num = int(input('Digite um numero: ')) 
+   numeros.append(num)# Adiciona o número digitado na lista
+print(numeros)
