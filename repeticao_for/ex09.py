11437a9cc7a19a555b6c3d0f5bfb22a5eed19097
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-03-05 07:32:19 -0300

    add exercicio

---
+++ repeticao_for/ex09.py
@@ -0,0 +1,8 @@
+inicio = int(input('Digite o numero: '))
+final = int(input('Digite o numero: '))
+pulo = int(input('Digite o numero: '))
+
+for c in range(inicio, final+1, pulo):
+    print(c)
+print('Fim')
+
