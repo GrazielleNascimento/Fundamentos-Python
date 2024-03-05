4ceb1d413c1e3e5efe153fae7e144908dce93048
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-03-05 07:32:34 -0300

    add exercicio

---
+++ repeticao_for/met_append.py
@@ -0,0 +1,9 @@
+lista = []        # Começa com uma lista vazia
+lista.append(1)   # Adiciona o número 1 no final da lista -> lista agora é [1]
+lista.append(2)   # Adiciona o número 2 no final da lista -> lista agora é [1, 2]
+lista.append(3)   # Adiciona o número 3 no final da lista -> lista agora é [1, 2, 3]
+
+print(lista)      # Saída: [1, 2, 3]
+
+#Cada vez que append é chamado, o estado da lista muda, 
+# adicionando um novo elemento ao seu final.
