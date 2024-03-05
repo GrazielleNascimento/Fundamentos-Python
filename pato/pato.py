0a3edc9a5ba2ab7fd72da5f9926ba508cfc93f1e
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-03-05 07:31:07 -0300

    add exercicio

---
+++ pato/pato.py
@@ -0,0 +1,9 @@
+class PatoBorracha:
+    def__init__(self, cor, tamanho, peso)
+    self.cor = cor
+    self.tamanho = tamanho
+    self.peso = peso 
+    
+    def esguicharAgua(self):
+    
+    pass
